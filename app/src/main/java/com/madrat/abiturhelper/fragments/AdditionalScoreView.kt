944bcc18f9a4cf002fc83a9f440e4cf9ff5e2bb5
MadRat <mischa.alpeew@yandex.ru>
2019-04-16 09:59:32 +0300

    Updated AdditionalScore, removed unused things

--- app/src/main/java/com/madrat/abiturhelper/fragments/AdditionalScoreView.kt
+++ app/src/main/java/com/madrat/abiturhelper/fragments/AdditionalScoreView.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.madrat.abiturhelper.R
 import com.madrat.abiturhelper.presenters.fragments.AdditionalScorePresenter
 import com.madrat.abiturhelper.interfaces.fragments.AdditionalScoreMVP
+import kotlinx.android.synthetic.main.custom_spinner.view.*
 import kotlinx.android.synthetic.main.fragment_score_additional.*
+import kotlinx.android.synthetic.main.fragment_score_additional.view.*
 
 class AdditionalScoreView : Fragment(), AdditionalScoreMVP.View {
     companion object { val instance = AdditionalScoreView() }
@@ -32,8 +35,15 @@ class AdditionalScoreView : Fragment(), AdditionalScoreMVP.View {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.additionalScoreTitle)
-        return inflater.inflate(R.layout.fragment_score_additional,
+        val view = inflater.inflate(R.layout.fragment_score_additional,
                                 container, false)
+
+        val spinnerItems = resources.getStringArray(R.array.additionalScoreSpinnerEntries)
+        val adapter = ArrayAdapter(context, R.layout.custom_spinner, spinnerItems)
+        adapter.setDropDownViewResource(R.layout.custom_spinner)
+
+        view.additionalScoreSpinner.adapter = adapter
+        return view
     }
 
     override fun setupMVP() {
