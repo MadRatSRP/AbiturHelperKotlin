3c19411b745a53aa3bf87307b76709e1d8c00ff6
MadRat <mischa.alpeew@yandex.ru>
2019-05-28 21:24:58 +0300

    Updated profile files(9)

--- app/src/main/java/com/madrat/abiturhelper/fragments/profile/CalculateUserPlacesView.kt
+++ app/src/main/java/com/madrat/abiturhelper/fragments/profile/CalculateUserPlacesView.kt
@@ -7,114 +7,42 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.madrat.abiturhelper.R
+import com.madrat.abiturhelper.adapter.SelectedSpecialtiesAdapter
 import com.madrat.abiturhelper.model.Specialty
 import com.madrat.abiturhelper.model.Student
 import com.madrat.abiturhelper.util.MyApplication
+import com.madrat.abiturhelper.util.linearManager
 import com.madrat.abiturhelper.util.showLog
+import kotlinx.android.synthetic.main.fragment_calculate_user_places.*
+import kotlinx.android.synthetic.main.fragment_calculate_user_places.view.*
 
 class CalculateUserPlacesView: Fragment() {
+    var adapter: SelectedSpecialtiesAdapter? = null
+
     val myApplication = MyApplication.instance
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         val selectedSpecialties = myApplication.returnSelectedSpecialties()
-
-        val selectedSpecialty = selectedSpecialties?.let { selectedSpecialties[0] }
-
-        val profileTerm = selectedSpecialty?.profileTerm
-
-        val facultyNumber = when(selectedSpecialty?.faculty) {
-            // УНТИ
-            "Учебно-научный технологический институт" -> 0
-            // ФЭУ
-            "Факультет экономики и управления" -> 1
-            // ФИТ
-            "Факультет информационных технологий" -> 2
-            // МТФ
-            "Механико-технологический факультет" -> 3
-            // УНИТ
-            "Учебно-научный институт транспорта" -> 4
-            // ФЭЭ
-            "Факультет энергетики и электроники" -> 5
-            else -> 7
-        }
-
-        val studentsOfFaculty = when (selectedSpecialty?.faculty) {
-            // УНТИ
-            "Учебно-научный технологический институт" -> myApplication.returnUNTI()
-            // ФЭУ
-            "Факультет экономики и управления" -> myApplication.returnFEU()
-            // ФИТ
-            "Факультет информационных технологий" -> myApplication.returnFIT()
-            // МТФ
-            "Механико-технологический факультет" -> myApplication.returnMTF()
-            // УНИТ
-            "Учебно-научный институт транспорта" -> myApplication.returnUNIT()
-            // ФЭЭ
-            "Факультет энергетики и электроники" -> myApplication.returnFEE()
-            else -> null
-        }
-
-        val specialtiesOfFaculty = getSpecialtiesListByPosition(facultyNumber)
-
-        val positionOfSpecialty = specialtiesOfFaculty?.indexOf(selectedSpecialty)
-        showLog("ПОЗИЦИЯ: $positionOfSpecialty")
-
-        val currentStudentsList = positionOfSpecialty?.let{ studentsOfFaculty?.get(positionOfSpecialty) }
-        showLog("РАЗМЕР: ${currentStudentsList?.size}")
-
-        val score = myApplication.returnScore()
-        val additionalScore = myApplication.returnAdditionalScore()
-
-        val student = score?.let {
-            additionalScore?.let { it1 ->
-                Student("007", "Алпеев", "Михаил", "Сергеевич",
-                        "####", "net", "Принято", "", "бак",
-                        "по конкурсу", " ", "", "",
-                        score.russian, score.maths, score.physics, score.computerScience, score.socialScience,
-                        it1, true, true, 0)
-            }
-        }
-        student?.let { currentStudentsList?.add(it) }
-        showLog("РАЗМЕР: ${currentStudentsList?.size}")
-
-        //var sortedCurrentStudentList: ArrayList<Student>? = null
-
-        when(profileTerm) {
-            "Физика" -> currentStudentsList?.sortBy { it.maths + it.physics + it.additionalScore }
-            "Обществознание" -> currentStudentsList?.sortBy { it.maths + it.socialScience + it.additionalScore }
-            "Информатика и ИКТ" -> currentStudentsList?.sortBy { it.maths + it.computerScience + it.additionalScore }
-        }
-
-        val newPosition = currentStudentsList?.indexOf(student)
-        showLog("ПОЗИЦИЯ: $newPosition")
+        selectedSpecialties?.let { showSelectedSpecialties(it) }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.calculateUserPlacesTitle)
         val view = inflater.inflate(R.layout.fragment_calculate_user_places,
                 container, false)
+
+        adapter = SelectedSpecialtiesAdapter()
+
+        view.calculateRecyclerView.adapter = adapter
+        view.calculateRecyclerView.linearManager()
+
         return view
     }
 
-    /*override*/ fun getSpecialtiesListByPosition(pos: Int): ArrayList<Specialty>? {
-        val faculties = myApplication.returnFaculties()
-
-        return when (pos) {
-            //УНТИ
-            0 -> faculties?.listUNTI
-            //ФЭУ
-            1 -> faculties?.listFEU
-            //ФИТ
-            2 -> faculties?.listFIT
-            //МТФ
-            3 -> faculties?.listMTF
-            //УНИТ
-            4 -> faculties?.listUNIT
-            //ФЭЭ
-            5 -> faculties?.listFEE
-            else -> null
-        }
+    /*override*/ fun showSelectedSpecialties(specialties: ArrayList<Specialty>) {
+        adapter?.updateSpecialtiesList(specialties)
+        calculateRecyclerView.adapter = adapter
     }
 }
