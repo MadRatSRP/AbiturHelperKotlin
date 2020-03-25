385bc8d503540f7cc60864258e6ddaafbecd1194
MadRat <mischa.alpeew@yandex.ru>
2020-03-25 21:23:06 +0300

    Adapters: added ViewBinding for CompleteSpecialtiesAdapter

--- app/src/main/java/com/madrat/abiturhelper/adapter/CompleteSpecialtiesAdapter.kt
+++ app/src/main/java/com/madrat/abiturhelper/adapter/CompleteSpecialtiesAdapter.kt
@@ -7,14 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.madrat.abiturhelper.databinding.ListCompleteSpecialtiesBinding
 import com.madrat.abiturhelper.model.Specialty
 import com.madrat.abiturhelper.util.inflate
-import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.list_complete_specialties.*
-import kotlinx.android.synthetic.main.list_specialties.specialtyAmountOfStatementsValue
-import kotlinx.android.synthetic.main.list_specialties.specialtyEntriesFreeValue
-import kotlinx.android.synthetic.main.list_specialties.specialtyEntriesTotalValue
-import kotlinx.android.synthetic.main.list_specialties.specialtyMinimalScoreText
-import kotlinx.android.synthetic.main.list_specialties.specialtyMinimalScoreValue
-import kotlinx.android.synthetic.main.list_specialties.specialtyName
 
 class CompleteSpecialtiesAdapter(itemStateArray: SparseBooleanArray?,
                                  chosenSpecialties: ArrayList<Specialty>?)
@@ -47,48 +39,45 @@ class CompleteSpecialtiesAdapter(itemStateArray: SparseBooleanArray?,
             = CompleteSpecialtiesHolder(parent.inflate(com.madrat.abiturhelper.R.layout.list_complete_specialties))
 
     override fun onBindViewHolder(holder: CompleteSpecialtiesHolder, position: Int){
-        val selectedSpecialty = specialties[position]
-
-        holder.bind(selectedSpecialty, position)
-        holder.completeCheckbox.setOnClickListener {
-            val adapterPosition = holder.adapterPosition
-            if (!itemStateArray.get(adapterPosition, false)) {
-                holder.completeCheckbox.isChecked = true
-                itemStateArray.put(adapterPosition, true)
-                selectedSpecialties.add(specialties[adapterPosition])
-            }
-            else  {
-                holder.completeCheckbox.isChecked = false
-                itemStateArray.put(adapterPosition, false)
-                selectedSpecialties.remove(specialties[adapterPosition])
-            }
-        }
+        holder.bind(specialties[position], position)
     }
 
     override fun getItemCount(): Int
             = specialties.size
 
-    inner class CompleteSpecialtiesHolder (override val containerView: View)
-        : RecyclerView.ViewHolder(containerView), LayoutContainer {
-        private val binding = ListCompleteSpecialtiesBinding.bind(containerView)
+    inner class CompleteSpecialtiesHolder (holderView: View): RecyclerView.ViewHolder(holderView) {
+        private val binding = ListCompleteSpecialtiesBinding.bind(holderView)
 
         fun bind(specialty: Specialty, position: Int) {
-            specialtyName.text = specialty.shortName
-            specialtyEntriesTotalValue.text = specialty.entriesTotal.toString()
-            specialtyEntriesFreeValue.text = specialty.entriesFree.toString()
-            specialtyAmountOfStatementsValue.text = specialty.amountOfStatements.toString()
-            specialtyMinimalScoreText.text = specialty.scoreTitle
-            specialtyMinimalScoreValue.text = specialty.minimalScore.toString()
+            with(binding) {
+                specialtyName.text = specialty.shortName
+                specialtyEntriesTotalValue.text = specialty.entriesTotal.toString()
+                specialtyEntriesFreeValue.text = specialty.entriesFree.toString()
+                specialtyAmountOfStatementsValue.text = specialty.amountOfStatements.toString()
+                specialtyMinimalScoreText.text = specialty.scoreTitle
+                specialtyMinimalScoreValue.text = specialty.minimalScore.toString()
+            }
 
             /*completeCheckbox.isChecked = false
 
             completeCheckbox.isChecked = itemStateArray.get(position, false)*/
 
-            completeCheckbox.isChecked = itemStateArray.get(position, false)
+            binding.completeCheckbox.isChecked = itemStateArray.get(position, false)
+
+            binding.completeCheckbox.setOnClickListener {
+                val adapterPosition = this.adapterPosition
+
+                if (!itemStateArray.get(adapterPosition, false)) {
+                    binding.completeCheckbox.isChecked = true
+                    itemStateArray.put(adapterPosition, true)
+                    selectedSpecialties.add(specialties[adapterPosition])
+                }
+                else  {
+                    binding.completeCheckbox.isChecked = false
+                    itemStateArray.put(adapterPosition, false)
+                    selectedSpecialties.remove(specialties[adapterPosition])
+                }
+            }
         }
-        /*fun setOnClickListener(onClickListener: View.OnClickListener) {
-            //containerView.setOnClickListener { onClickListener }
-            containerView.setOnClickListener(onClickListener)
-        }*/
     }
 }
